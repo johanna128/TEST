d895074e456d5597b91306675ae35a2648ae7395
johanna128 <71490640+johanna128@users.noreply.github.com>
2020-09-19 21:16:45 +0400

    Bug fix

--- TEST/Startup.cs
+++ TEST/Startup.cs
@@ -20,37 +20,33 @@ namespace TEST
             ApplicationDbContext context = new ApplicationDbContext();
 
             var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
-            //var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
 
-
-            // In Startup iam creating first Admin Role and creating a default Admin User     
             if (!roleManager.RoleExists("Admin"))
             {
 
-                // first we create Admin rool    
+                // Create Admin role    
                 var role = new IdentityRole();
                 role.Name = "Admin";
                 roleManager.Create(role);
 
-                ////Here we create a Admin super user who will maintain the website                   
+                //create Admin super user who will maintain the website              
+                var user = new ApplicationUser();
+                user.UserName = "test2@test.com";
+                user.Email = "test2@test.com";
 
-                //var user = new ApplicationUser();
-                //user.UserName = "shanu";
-                //user.Email = "syedshanumcain@gmail.com";
+                string userPWD = "Test1!";
 
-                //string userPWD = "A@Z200711";
+                var chkUser = UserManager.Create(user, userPWD);
 
-                //var chkUser = UserManager.Create(user, userPWD);
-
-                ////Add default User to Role Admin    
-                //if (chkUser.Succeeded)
-                //{
-                //    var result1 = UserManager.AddToRole(user.Id, "Admin");
-
-                //}
+                //Add default User to Admin Role     
+                if (chkUser.Succeeded)
+                {
+                    UserManager.AddToRole(user.Id, "Admin");
+                }
             }
 
-            // Creating Operator role     
+            // Creat Operator role     
             if (!roleManager.RoleExists("Operator"))
             {
                 var role = new IdentityRole();
